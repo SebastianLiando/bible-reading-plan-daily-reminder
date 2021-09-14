a7fa99c0dfa76719000d91bbc865b1afa9004248
Sebastian <sebastianliando@gmail.com>
2021-09-15 00:16:37 +0800

    Handle no plan

--- main.py
+++ main.py
@@ -9,53 +9,60 @@ import csv
 
 from src.utils import get_superscript
 
-# Read reading plan data.
-rows = []
-with open(get_asset('plan.csv')) as file:
-    reader = csv.reader(file)
+def main():   
+    # Read reading plan data.
+    rows = []
+    with open(get_asset('plan.csv')) as file:
+        reader = csv.reader(file)
 
-    # Exclude header
-    rows = list(reader)[1:]
-plan_manager = PlanManager(rows)
+        # Exclude header
+        rows = list(reader)[1:]
+    plan_manager = PlanManager(rows)
 
-# Get today's reading plan.
-task_today = plan_manager.get_task_at(date=datetime(2021, 9, 20).date())
+    # Get today's reading plan.
+    task_today = plan_manager.get_task_at(datetime.now().date())
 
-# Prepare Bible.
-bible = Bible()
+    if task_today is None:
+        return
 
-# Get verses for today.
-today_verses = bible.get_verses_from_chapter(
-    task_today.book,
-    task_today.chapter,
-    task_today.start_verse,
-    task_today.end_verse
-)
+    # Prepare Bible.
+    bible = Bible()
 
-# Add the verse numbers to each verse.
-for i in range(0, len(today_verses)):
-    verse_no = get_superscript(i + 1)
+    # Get verses for today.
+    today_verses = bible.get_verses_from_chapter(
+        task_today.book,
+        task_today.chapter,
+        task_today.start_verse,
+        task_today.end_verse
+    )
 
-    today_verses[i] = f"{verse_no} {today_verses[i]}"
+    # Add the verse numbers to each verse.
+    for i in range(0, len(today_verses)):
+        verse_no = get_superscript(i + 1)
 
-# Which chapter and book the verses are from
-reading_chapter = f'📚<b>{task_today.book.upper()} {task_today.chapter}</b>'
+        today_verses[i] = f"{verse_no} {today_verses[i]}"
 
-# Join the verses into a single string.
-verses = '\n'.join(today_verses)
+    # Which chapter and book the verses are from
+    reading_chapter = f'📚<b>{task_today.book.upper()} {task_today.chapter}</b>'
 
-# The message lines to be send by telegram bot.
-message_lines = [
-    "Hi friends 🖐! Here's the reading plan for today.",
-    '\n',
-    reading_chapter,
-    verses
-]
+    # Join the verses into a single string.
+    verses = '\n'.join(today_verses)
 
-# Start the bot
-bot = telegram.Bot(token=TOKEN)
-bot.send_message(
-    chat_id=278794854,
-    text='\n'.join(message_lines),
-    parse_mode=telegram.ParseMode.HTML
-)
+    # The message lines to be send by telegram bot.
+    message_lines = [
+        "Hi friends 🖐! Here's the reading plan for today.",
+        '\n',
+        reading_chapter,
+        verses
+    ]
+
+    # Start the bot
+    bot = telegram.Bot(token=TOKEN)
+    bot.send_message(
+        chat_id=278794854,
+        text='\n'.join(message_lines),
+        parse_mode=telegram.ParseMode.HTML
+    )
+
+if __name__ == "__main__":
+    main()
